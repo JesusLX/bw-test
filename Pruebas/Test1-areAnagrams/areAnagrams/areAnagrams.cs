3762aced2d6bdb3ce3f460cd51334848f80765b2
JesusLX <jpmx96@gmail.com>
2023-07-17 08:31:27 +0200

    Incluyendo test 1 y 2

---
+++ Pruebas/Test1-areAnagrams/areAnagrams/areAnagrams.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace areAnagrams {
+    internal class AreAnagrams {
+
+        /// <summary>
+        /// Checks if two words are anagrams of each other.
+        /// An anagram is a word formed by rearranging the letters of another word using all the letters only once.
+        /// </summary>
+        /// <param name="word1">The first word to check.</param>
+        /// <param name="word2">The second word to check.</param>
+        /// <returns>True if the words are anagrams; otherwise, false.</returns>
+        public static bool AreStringsAnagrams(string a, string b) {
+            bool areAnagrams = false;
+
+            if (a.Length == b.Length) {
+                string aSorted = new(
+                    a.ToLower().
+                    OrderBy(ch => ch).
+                    ToArray()
+                );
+                string bSorted = new(
+                    b.ToLower().
+                    OrderBy(ch => ch).
+                    ToArray()
+                );
+
+                areAnagrams = aSorted == bSorted;
+            }
+            return areAnagrams;
+        }
+
+        public static void Main(string[] args) {
+            Console.WriteLine(AreStringsAnagrams("Momdad", "Dadmom"));
+        }
+
+    }
+}
---
+++ Pruebas/Test2-Run/run/Run.cs
@@ -0,0 +1,50 @@
+﻿namespace run {
+    //Escribe una función que encuentre el índice(basado en 0) de la carrera más larga en una string. Una carrera es una secuencia consecutiva del mismo carácter.Si hay más de una carrera con la máxima longitud, devuelve el índice de la primera.
+    //Por ejemplo, IndexOfLongestRun("abbcccddddcccbba") debería devolver 6 ya que la carrera más larga es “dddd” y aparece por primera vez en el índice 6.
+
+    using System;
+
+    public class Run {
+
+        /// <summary>
+        /// Finds the index of the longest run in a string.
+        /// A run is a consecutive sequence of the same character.
+        /// If there are multiple runs with the maximum length, it returns the index of the first one.
+        /// </summary>
+        /// <param name="str">The string in which to find the longest run.</param>
+        /// <returns>The index of the start of the longest run.</returns>
+        public static int IndexOfLongestRun(string str) {
+
+            Runner currentRunner = new(0, '\0',-1);
+            Runner longestRunner = currentRunner;
+            
+            for (int i = 0; i < str.Length; i++) {
+                if (currentRunner.Name != str[i]) {
+                    currentRunner = new(i, str[i]);
+                }
+                currentRunner.Steps++;
+                    
+                if(longestRunner.Steps < currentRunner.Steps) longestRunner = currentRunner;
+            }
+            return longestRunner.InitPos;
+        }
+
+        public static void Main(string[] args) {
+            Console.WriteLine(IndexOfLongestRun("abbcccddddcccbba"));
+        }
+    }
+
+    public struct Runner {
+        public Runner(int initPos, char runnerName, int defaultLeng = 0) {
+            InitPos = initPos;
+            Name = runnerName;
+            Steps = defaultLeng;
+        }
+
+        public int InitPos { get; }
+        public char Name { get; }
+        public int Steps { get; set; }
+
+        
+    }
+}
